6f9da3c7c843cb6aaf059af5ecf046f7ce41c656
bjeong <bjeong@ymail.com>
2013-07-29 17:14:51 -0700

    workspace items added + static folder support

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello water!')
 
 app = webapp2.WSGIApplication([
