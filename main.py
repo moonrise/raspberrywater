8afb5f4c889c0937c5325c7ea47fbc1477ba8da0
bjeong <bjeong@ymail.com>
2013-07-29 17:43:58 -0700

    google user authentication added

--- main.py
+++ main.py
@@ -14,12 +14,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
 import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        user = users.get_current_user()
+
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello water!')
+        if user:
+            self.response.write('Hello, ' + user.nickname())
+        else:
+            #self.response.write('Hello water!')
+            self.redirect(users.create_login_url(self.request.uri))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
