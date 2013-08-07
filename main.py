bcd1938b7165d8bc8f2258e510f4d2486c22179a
bjeong <bjeong@ymail.com>
2013-08-06 18:18:44 -0700

    jinja2 template test

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ MAIN_HTML = """\
         <p><a href="/test/user">User</a></p>
         <p><a href="/test/form">Form</a></p>
         <p><a href="/test/hrd">HRD (High Replication Datastore)</a></p>
+        <p><a href="/test/hrd2">HRD2 (High Replication Datastore with Jinja2 Templating)</a></p>
     </body>
 </html>
 """
